593282fda1af942c18c593f67a3b9a1c68c3753a
Mr.-x- <mistax2.0z@gmail.com>
2026-09-01 00:02:21 +0530

    Update movie_downloader.py

--- movie_downloader.py
+++ movie_downloader.py
@@ -12,7 +12,7 @@ from urllib.parse import quote
 import requests
 
 
-BASE_URL = "http://movie.mrxpro.eu.cc:8080"
+BASE_URL = "http://movie.cloud.eu.cc:8080"
 DEFAULT_PROXY_URL = "http://127.0.0.1:10808"
 DISK_CACHE_SIZE = 1024 * 1024
 DEFAULT_WORKERS = 1
